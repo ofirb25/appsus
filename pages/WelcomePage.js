e94c562e82fe04ece493ee7f47738d4a1c033c11
OfirBlumshtein <ofzir11@gmail.com>
2017-12-10 12:06:56 +0200

    lll

--- pages/WelcomePage.js
+++ pages/WelcomePage.js
@@ -1,7 +1,17 @@
-export default {
-    template : `
-        <section>
-            hello!!!
-        </section>
-    `
-}
+export default { template : `
+<section>
+    <h1 class="title is-1">Welcome To Appsus</h1>
+
+    <section>
+        <div class="app-wrapper">
+            <img src="assets/mapapp.gif" alt="">
+        </div>
+        <div class="app-wrapper">
+            <img src="assets/mailsapp.gif" alt="">
+        </div>
+        <i class="fa fa-envelope title is-1" aria-hidden="true"></i>
+        <i class="fa fa-note title is-1" aria-hidden="true"></i>
+
+    </section>
+</section>
+` }
